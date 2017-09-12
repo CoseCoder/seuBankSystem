7b960d3b03bd3101b439c26861cb586c4d43d1e6
CoseCoder <838491325@qq.com>
2017-09-12 22:09:16 +0800

    v0.15

--- src/main/java/group_seven/testdemo/LoadToHive.scala
+++ src/main/java/group_seven/testdemo/LoadToHive.scala
@@ -8,82 +8,84 @@ object LoadToHive {
     val spark = SparkSession.builder().appName("LoadToHive").master("local").enableHiveSupport().getOrCreate()
     val sc = spark.sparkContext
 
-    //生成广播变量
 
+    //将导入的文件数据处理成RDD
+    //将user_info_train.txt的每一行处理成（用户 id,性别,职业,教育程度,婚姻状态,户口类型）
     val user_info = sc.textFile("src/main/resources/data/user_info_train.txt").map(_.split(","))
       .map(m => (m(0).toInt, m(1).toInt, m(2).toInt, m(3).toInt, m(4).toInt, m(5).toInt))
 
+    //将overdue_train.txt的每一行处理成（用户 id,样本标签）
     val overdue = sc.textFile("src/main/resources/data/overdue_train.txt").map(_.split(","))
       .map(m => (m(0).toInt, m(1).toInt))
 
+    //将bank_detail_train.txt的每一行处理成（用户 id,时间戳,交易类型,交易金额,工资收入标记）
     val bank_detail = sc.textFile("src/main/resources/data/bank_detail_train.txt").map(_.split(","))
       .map(m => (m(0).toInt, m(1), m(2).toInt, m(3).toDouble, m(4).toInt))
 
+    //将browse_history_train.txt的每一行处理成（用户 id,时间戳,浏览行为数据,浏览子行为编号）
     val browse_history = sc.textFile("src/main/resources/data/browse_history_train.txt").map(_.split(","))
       .map(m => (m(0).toInt, m(1), m(2).toInt, m(3).toInt))
 
+    //将loan_time_train.txt的每一行处理成（用户 id,放款时间）
     val loan_time = sc.textFile("src/main/resources/data/loan_time_train.txt").map(_.split(","))
       .map(m => (m(0).toInt, m(1)))
 
+    //将bill_detail_train.txt的每一行处理成（用户 id,账单时间戳,银行 id,上期账单金额,上期还款金额,信用卡额度,本期账单余额,本期账单最低还款额,消费笔数,本期账单金额,调整金额,循环利息,可用金额,预借现金额
+    //    度,还款状态）
     val bill_detail = sc.textFile("src/main/resources/data/bill_detail_train.txt").map(_.split(","))
       .map(m => (m(0).toInt, m(1), m(2).toInt, m(3).toDouble, m(4).toDouble,
         m(5).toDouble, m(6).toDouble, m(7).toDouble, m(8).toInt, m(9).toDouble
         , m(10).toDouble, m(11).toDouble, m(12).toDouble, m(13).toDouble, m(14).toInt))
 
 
+    //将RDD导入Hive的数据表中
     val hiveContext = new HiveContext(sc)
     import hiveContext.implicits._
 
+    //创建选定数据库
     hiveContext.sql("create database IF NOT EXISTS bank")
     hiveContext.sql("use bank")
-    //    hiveContext.sql("truncate table user_info")
-    var df1 = user_info.toDF("id", "gender", "job", "education", "marriage", "residence").registerTempTable("user_info")
 
+    //创建数据表user_info并插入数据
+    var df1 = user_info.toDF("id", "gender", "job",
+      "education", "marriage", "residence").registerTempTable("user_info")
     hiveContext.sql("create table IF NOT EXISTS user_info as " +
       "select * from user_info")
+    //    hiveContext.sql("select * from user_info").show()
 
-    hiveContext.sql("select * from user_info").show()
-
-    //    hiveContext.sql("truncate table overdue")
+    //创建数据表overdue并插入数据
     var df2 = overdue.toDF("id", "label").registerTempTable("overdue")
-
     hiveContext.sql("create table IF NOT EXISTS overdue as " +
       "select * from overdue")
+    //    hiveContext.sql("select * from overdue").show()
 
-    hiveContext.sql("select * from overdue").show()
-
-    //    hiveContext.sql("truncate table bank_detail")
-    var df3 = bank_detail.toDF("id", "time", "billtype", "money", "wageflag").registerTempTable("bank_detail")
-
+    //创建数据表bank_detail并插入数据
+    var df3 = bank_detail.toDF("id", "time", "billtype",
+      "money", "wageflag").registerTempTable("bank_detail")
     hiveContext.sql("create table IF NOT EXISTS bank_detail as " +
       "select * from bank_detail")
+    //    hiveContext.sql("select * from bank_detail").show()
 
-    hiveContext.sql("select * from bank_detail").show()
-
-    //    hiveContext.sql("truncate table browse_history")
-    var df4 = browse_history.toDF("id", "time", "browsedata", "browsenumber").registerTempTable("browse_history")
-
+    //创建数据表browse_history并插入数据
+    var df4 = browse_history.toDF("id", "time", "browsedata",
+      "browsenumber").registerTempTable("browse_history")
     hiveContext.sql("create table IF NOT EXISTS browse_history as " +
       "select * from browse_history")
+    //    hiveContext.sql("select * from browse_history").show()
 
-    hiveContext.sql("select * from browse_history").show()
-
-    //    hiveContext.sql("truncate table loan_time")
+    //创建数据表loan_time并插入数据
     var df5 = loan_time.toDF("id", "time").registerTempTable("loan_time")
-
     hiveContext.sql("create table IF NOT EXISTS loan_time as " +
       "select * from loan_time")
+    //    hiveContext.sql("select * from loan_time").show()
 
-    hiveContext.sql("select * from loan_time").show()
-
-    //    hiveContext.sql("truncate table bill_detail")
+    //创建数据表bill_detail并插入数据
     var df6 = bill_detail.toDF("id", "time", "bankid", "lastbillmoney",
-      "lastrepaymoney", "creditlimit", "balance", "leastrepaymoney", "salesnumber",
-      "billmoney", "adjustmoney", "cycleinterest", "available", "cashadvancelimit", "repayatatus").registerTempTable("bill_detail")
-
+      "lastrepaymoney", "creditlimit", "balance", "leastrepaymoney",
+      "salesnumber", "billmoney", "adjustmoney", "cycleinterest",
+      "available", "cashadvancelimit", "repayatatus").registerTempTable("bill_detail")
     hiveContext.sql("create table IF NOT EXISTS bill_detail as " +
       "select * from bill_detail")
-
-    hiveContext.sql("select * from bill_detail").show()
+    //    hiveContext.sql("select * from bill_detail").show()
   }
 }
--- src/main/java/group_seven/testdemo/data_b_bill.scala
+++ src/main/java/group_seven/testdemo/data_b_bill.scala
@@ -10,50 +10,65 @@ object data_b_bill {
     val spark = SparkSession.builder().appName("data_b_bill").master("local").enableHiveSupport().getOrCreate()
     val sc = spark.sparkContext
 
-    //生成广播变量
-    //    val fields = List("BillNumber","Account","Money","BillDate","BillType","BillExInfo","BankSiteID")
-    //    val bcfields = sc.broadcast(fields)
+    //获取文件中的数据，并转化成RDD[Array[String]]
+    val bill_data = sc.textFile("src/main/resources/data/data_b_bill.csv").map(_.split(","))
 
-    val bill_data = sc.textFile("src/main/resources/data/data_b_bill.csv").map(_.split(",")).filter(m => (m != ""))
+    //删除bill_Data中每个Array[]中每一个String对象头尾的双引号,且把RDD[Array[String]]
+    // 转换成RDD[(String.String,Double.String,String,String)]
+    // 并将每行的第三个元素(m(3)即时间字符串)中的"/"替换成"-"
     val data00 = bill_data.map(row => (row.map(colume => (colume.replace("\"", "")))))
       .map(m => (m(0), m(1), m(2).toDouble, m(3).replace("/", "-"), m(4), m(5)))
 
-    val data01 = data00.filter(m => (m._4.length == 17)).map(m => (m._1, m._2, m._3, m._4.substring(0, 5) + "0" + m._4.substring(5, 17), m._5, m._6))
-    val data02 = data00.filter(m => (m._4.length == 18 && m._4.lastIndexOf("-") == 6)).map(m => (m._1, m._2, m._3, m._4.substring(0, 5) + "0" + m._4.substring(5, 18), m._5, m._6))
-    val data03 = data00.filter(m => (m._4.length == 18 && m._4.lastIndexOf("-") == 7)).union(data01).map(m => (m._1, m._2, m._3, m._4.substring(0, 8) + "0" + m._4.substring(8, 18), m._5, m._6))
+    //修正每行的第三个元素(m(3)即时间字符串)的格式,"yyyy-mm-dd HH:MM:SS"
+    //将"yyyy-m-d HH:MM:SS"转换成"yyyy-mm-d HH:MM:SS",即在月份前补0
+    val data01 = data00.filter(m => (m._4.length == 17)).
+      map(m => (m._1, m._2, m._3, m._4.substring(0, 5) + "0" + m._4.substring(5, 17),
+        m._5, m._6))
+
+    //将"yyyy-m-dd HH:MM:SS"转换成"yyyy-mm-dd HH:MM:SS",,即在月份前补0
+    val data02 = data00.filter(m => (m._4.length == 18 && m._4.lastIndexOf("-") == 6))
+      .map(m => (m._1, m._2, m._3, m._4.substring(0, 5) + "0" + m._4.substring(5, 18),
+        m._5, m._6))
+
+    //将"yyyy-mm-d HH:MM:SS"转换成"yyyy-mm-dd HH:MM:SS",即在日期前补0
+    val data03 = data00.filter(m => (m._4.length == 18 && m._4.lastIndexOf("-") == 7))
+      .union(data01)
+      .map(m => (m._1, m._2, m._3, m._4.substring(0, 8) + "0" + m._4.substring(8, 18), m._5, m._6))
+
+    //将前面修正过后的RDD与格式正确的RDD合并
     val data = data00.filter(m => (m._4.length == 19)).union(data02).union(data03).sortBy(m => m._4)
-    //    data.foreach(println)
 
-//    println("全部交易金额都是大额的整数（>=2000）")
+    //找出交易金额为大额整数的交易信息(>=2000)
     val bigInteger = data.filter(row => (row._3 >= 2000 && row._3 % 100 == 0))
-      .map(m=>(m._1,m._2,m._3,m._4,m._5,m._6,"大额整数"))
-    //      .foreach(println)
+      .map(m => (m._1, m._2, m._3, m._4, m._5, m._6, "大额整数"))
 
-    //    println("交易金额差距过大（>=3000）")
-    //    data.keyBy(_._2).groupByKey()
-    //      .filter(m=>((m._2.map(m=>m._3).max-m._2.map(m=>m._3).min)>=3000))
-    //      .flatMap(m=>(m._2)).map(m=>(m._1,m._2,m._3,m._4,m._5,m._6))
-    //      .foreach(println)
+    //找出交易金额差距过大的交易信息(>=3000)
+    data.keyBy(_._2).groupByKey()
+      .filter(m => ((m._2.map(m => m._3).max - m._2.map(m => m._3).min) >= 3000))
+      .flatMap(m => (m._2)).map(m => (m._1, m._2, m._3, m._4, m._5, m._6))
+      .foreach(println)
 
-//    println("交易的时间不对")
+    //找出交易时间异常的交易信息(<=8:00,>=22:00)
     val exTime = data.filter(m => (m._1.substring(8, 10).toInt < 8 || m._1.substring(8, 10).toInt > 22))
-      .map(m=>(m._1,m._2,m._3,m._4,m._5,m._6,"异常时间"))
-    //      .foreach(println)
+      .map(m => (m._1, m._2, m._3, m._4, m._5, m._6, "异常时间"))
 
+    //将得到的数据导入MySQL的数据表exceptionaccount中
     val sqlContext = new SQLContext(sc)
     import sqlContext.implicits._
 
-    var df=bigInteger.union(exTime).toDF("BillNumber","Account","Money","BillDate","BillExInfo","BankSiteID","ExReason")
-    df.show()
+    var df = bigInteger.union(exTime)
+      .toDF("BillNumber", "Account", "Money", "BillDate", "BillExInfo", "BankSiteID", "ExReason")
+
+    //    df.show()
+
     val dfWriter = df.write.mode("append")
 
     val prop = new Properties()
     prop.put("user", "root")
     prop.put("password", "root")
-//    prop.put("useUnicode","true")
-//    prop.put("characterEncoding","utf-8")
     prop.put("transformedBitIsBoolean", "true")
     prop.put("useSSL", "false")
-    dfWriter.jdbc("jdbc:mysql://localhost:3306/bank"," exceptionaccount", prop)
+
+    dfWriter.jdbc("jdbc:mysql://localhost:3306/bank", " exceptionaccount", prop)
   }
 }
