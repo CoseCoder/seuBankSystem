9666f5c2c45cbb88b86bb2cf05e6d380dcdfb711
CoseCoder <838491325@qq.com>
2017-09-14 11:35:39 +0800

    v0.16

--- src/main/java/group_seven/testdemo/LogisticRegression.scala
+++ src/main/java/group_seven/testdemo/LogisticRegression.scala
@@ -17,6 +17,8 @@ object LogisticRegression {
     val sc = spark.sparkContext
 
     spark.sql("use bank")
+
+    //获取hive数据表中数据
     val rs00 = spark.sql("select overdue.id,max(overdue.label),max(user_info.gender)," +
       "max(user_info.job),max(user_info.education),max(user_info.marriage)," +
       "max(user_info.residence),count(browse_history.*)" +
@@ -27,10 +29,7 @@ object LogisticRegression {
       "avg(bill_detail.billmoney),avg(bill_detail.repayatatus) from overdue join bill_detail " +
       "on overdue.id=bill_detail.id group by overdue.id")
 
-    //.createTempView("bank")
-    //    .show(50),m(1)
-    //    rs.write.save("src/main/resources/data/rs01.txt")
-    //    val rs = spark.read.load("src/main/resources/data/rs01.txt")
+    //处理数据格式，转换成RDD[(Int,Array[Double])]
     val rs = rs00.join(rs01, "id")
       .rdd.map(m => (m(1).toString.toInt, Array(m(2).toString.toDouble,
       m(3).toString.toDouble, m(4).toString.toDouble, m(5).toString.toDouble,
@@ -38,17 +37,9 @@ object LogisticRegression {
       m(9).toString.toDouble, m(10).toString.toDouble, m(11).toString.toDouble,
       m(12).toString.toDouble, m(13).toString.toDouble, m(14).toString.toDouble)))
 
-    //        var data = sc.textFile("src/main/resources/data/sample_libsvm_data.txt")
-    //          .map(m => m.split("\t").map(_.toDouble))
-    //          .map(m => (m(0).toInt, Array(m(1), m(2), m(3), m(4), m(5), m(6))))
-
+    //转换成labeledPoint格式
     var data = rs.map(m => (LabeledPoint(m._1, Vectors.dense(m._2))))
     data.take(10).foreach(println)
-
-    //    MLUtils.saveAsLibSVMFile(lines,"src/main/resources/data/libsvm_data.txt")
-
-    // Load training data in LIBSVM format.
-    //    val data = MLUtils.loadLibSVMFile(sc, "src/main/resources/data/libsvm_data.txt")
 
     // Split data into training (60%) and test (40%).
     val splits = data.randomSplit(Array(0.7, 0.3), seed = 11L)
@@ -72,7 +63,7 @@ object LogisticRegression {
     println(s"Accuracy = $accuracy")
 
     // Save and load model
-    model.save(sc, "src/main/resources/data/scalaLogisticRegressionWithLBFGSModel")
+    //    model.save(sc, "src/main/resources/data/scalaLogisticRegressionWithLBFGSModel")
     //    val sameModel = LogisticRegressionModel.load(sc,
     //      "src/main/resources/data/scalaLogisticRegressionWithLBFGSModel")
     spark.stop()
